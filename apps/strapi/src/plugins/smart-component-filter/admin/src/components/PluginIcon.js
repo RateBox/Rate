55604552d42cda9ed7988b6846cfe9e785fcfbde
JOY <anh@metados.com>
2025-06-16 11:57:12 +0700

    * Remove JS file

--- apps/strapi/src/plugins/smart-component-filter/admin/src/components/PluginIcon.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PluginIcon = void 0;
-const icons_1 = require("@strapi/icons");
-const PluginIcon = () => <icons_1.PuzzlePiece />;
-exports.PluginIcon = PluginIcon;
