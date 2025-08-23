80966588697d9ae42ff78cb458225135d168a9a6
JOY <anh@metados.com>
2025-08-23 10:28:49 +0700

    chore(strapi): add eslint script, typecheck and lint-staged for src

---
+++ apps/strapi/.lintstagedrc.js
@@ -0,0 +1,13 @@
+const path = require('path')
+
+const buildEslintCommand = (filenames) =>
+  `eslint --fix --max-warnings=0 ${filenames
+    .filter((f) => f.endsWith('.ts') || f.endsWith('.tsx') || f.endsWith('.js'))
+    .map((f) => path.relative(process.cwd(), f))
+    .join(' ')}`
+
+module.exports = {
+  'src/**/*.{ts,tsx,js}': [buildEslintCommand],
+}
+
+
